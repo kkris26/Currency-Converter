b4d49854a281ceff61b4f332fbc78ca71bae3d5e
kkris26 <krisnuartha09@gmail.com>
2025-06-12 17:11:19 +0800

    clg response

--- api/currency.js
+++ api/currency.js
@@ -4,6 +4,7 @@ const apiCurrencies = async (req, res) => {
 
   try {
     const response = await fetch(API_CURRENCIES + API_KEY);
+    console.log(API_CURRENCIES + API_KEY);
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
