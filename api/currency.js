070c0f7b65977042b13e38e02dcd4f0928522a66
kkris26 <krisnuartha09@gmail.com>
2025-06-12 17:24:20 +0800

    test 2

--- api/currency.js
+++ api/currency.js
@@ -1,9 +1,9 @@
-const apiCurrencies = async (req, res) => {
+export default async function apiCurrencies(req, res) {
   const API_KEY = process.env.CURRENCIES_API_KEY;
   const API_CURRENCIES = process.env.API_CURRENCIES;
 
   try {
-    const response = await fetch(API_CURRENCIES + API_KEY);
+    const response = await fetch(`${API_CURRENCIES}${API_KEY}`);
 
     const data = await response.json();
     res.status(200).json(data);
@@ -12,6 +12,4 @@ const apiCurrencies = async (req, res) => {
       error: `Failed : ${API_CURRENCIES}${API_KEY}`,
     });
   }
-};
-
-export default apiCurrencies;
+}
